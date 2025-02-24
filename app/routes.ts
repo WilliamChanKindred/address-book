22e351b117fc3fb3d1ca6b8f4ab131dd7608ff9f
William Chan <william.chan@kindredgroup.com>
2025-02-24 11:27:42 +1100

    add contacts and home route

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,6 @@
-import type { RouteConfig } from "@react-router/dev/routes";
+import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
-export default [] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    route("contacts/:contactId", "routes/contact.tsx"),
+] satisfies RouteConfig;
